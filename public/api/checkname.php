6449fe70c19d036c4d25b309d4a1671c13268561
dante <dantinr@gmail.com>
2021-04-13 11:29:24 +0800

    test

--- public/api/checkname.php
+++ public/api/checkname.php
@@ -4,6 +4,15 @@
         "李四",
     ];
 
+    if(empty($_GET)){
+        $response = [
+            "errno" => 40001,
+            "msg"   => "缺少name参数"
+        ];
+    }
+
+    echo json_encode($response,JSON_UNESCAPED_UNICODE);die;
+
     if(in_array($_GET['name'],$names)){
         $response = [
             "errno" => 30001,
